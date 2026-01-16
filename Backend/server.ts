aca2164260233893071c28ec97e00f052bf3d399
Rajat <149293659+Rajat-0707@users.noreply.github.com>
2026-01-16 16:33:09 +0530

    Update server.ts

--- Backend/server.ts
+++ Backend/server.ts
@@ -11,8 +11,7 @@ import viewcapsuleRoute from "./routes/viewcapsule";
 import createcapsuleRoute from "./routes/createcapsule";
 
 const app = express();
-const port = 3000;
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.use((req, res, next) => {
